37813e22c7521323c4a6f636b9c1f14e2612edd3
Valeria-Sem <lerasemenenya@gmail.com>
2021-10-25 16:40:45 +0300

    create validation

--- src/main/java/com/epam/esm/service/validator/TagValidator.java
+++ src/main/java/com/epam/esm/service/validator/TagValidator.java
@@ -3,7 +3,7 @@ package com.epam.esm.service.validator;
 import org.apache.commons.lang3.StringUtils;
 
 public class TagValidator {
-    private static final String TAG_REGEX = "[a-zA-Z]{3,40}$";
+    private static final String TAG_REGEX = "[a-zA-Z]{3,40}";
 
     public void validateTagName(String name) throws ValidatorException{
         if(StringUtils.isBlank(name) || !name.matches(TAG_REGEX)){
